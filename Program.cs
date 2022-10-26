28a837f998b14ae81a257d0c130f824e33423d85
Tea <tea.krcmar@gmail.com>
2022-10-26 17:15:33 +0200

    Commit poruke iz programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Tee Krcmar.");
+        }
+    }
+}
